bc1772d00667d7845cca02a9a9660d0f11679c3c
michuW93 <michal.walkowiak93@gmail.com>
2020-05-21 19:28:08 +0200

    init

---
+++ src/main/java/com/example/demo/DemoWithH2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWithH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWithH2Application.class, args);
+	}
+
+}
