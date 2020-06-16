acde5492f57f6997f737a404bf487e0f20900a5d
michuW93 <michal.walkowiak93@gmail.com>
2020-06-16 21:28:18 +0200

    persist, find, remove, transactions

--- src/main/java/com/example/demo/DemoWithH2Application.java
+++ src/main/java/com/example/demo/DemoWithH2Application.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
 import com.example.demo.Employee.Employee;
+import com.example.demo.Employee.EmployeeServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 @SpringBootApplication
@@ -18,11 +18,11 @@ public class DemoWithH2Application {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeService");
         EntityManager em = emf.createEntityManager();
 
-        createEmployee(em, "Michal", "930818", 1L);
+        EmployeeServiceImpl employeeService = new EmployeeServiceImpl(null, em);
+        employeeService.createEmployee( "Michal", "930818", 1L);
 
-        Employee employee1 = em.find(Employee.class, 1L);
-        System.out.println(employee1.getFirstName());
-        removeEmployee(em, employee1.getEmployee_id());
+        Employee employee1 = employeeService.findEmployeeById(1L);
+        employeeService.removeEmployee(employee1.getEmployee_id());
 
         Employee removedEmployee = em.find(Employee.class, 1L);
         if (removedEmployee != null) {
@@ -31,32 +31,8 @@ public class DemoWithH2Application {
             System.out.println("Employee not found");
         }
 
-        changeEmployeeName(em, 2, "Edward");
-    }
+        employeeService.changeEmployeeName(2, "Edward");
 
-    private static void createEmployee(EntityManager em, String firstName, String pesel, long departmentId){
-        Employee employee = new Employee(firstName, pesel, departmentId);
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-        em.persist(employee);
-        transaction.commit();
-    }
-
-    private static void removeEmployee(EntityManager em, long employeeId) {
-        Employee emp = em.find(Employee.class, employeeId);
-        if (emp != null) {
-            em.remove(emp);
-        }
-    }
-
-    private static Employee changeEmployeeName(EntityManager em, long id, String firstName) {
-        Employee emp = em.find(Employee.class, id);
-        if (emp != null) {
-            EntityTransaction transaction = em.getTransaction();
-            transaction.begin();
-            emp.setFirstName(firstName);
-            transaction.commit();
-        }
-        return emp;
+        employeeService.findAllEmployees();
     }
 }
--- src/main/java/com/example/demo/Employee/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/Employee/EmployeeServiceImpl.java
@@ -3,15 +3,20 @@ package com.example.demo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository repository;
+    private final EntityManager em;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeRepository repository) {
+    public EmployeeServiceImpl(EmployeeRepository repository, EntityManager entityManager) {
         this.repository = repository;
+        em = entityManager;
     }
 
     @Override
@@ -29,4 +34,45 @@ public class EmployeeServiceImpl implements EmployeeService {
         return repository.findByFirstNameAndDepartment(firstName);
     }
 
+    public void findAllEmployees() {
+        TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employee e", Employee.class);
+        List<Employee> emps = query.getResultList();
+        System.out.println("Num of employees: " + emps.size());
+    }
+
+    public void createEmployee(String firstName, String pesel, long departmentId) {
+        Employee employee = new Employee(firstName, pesel, departmentId);
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(employee);
+        transaction.commit();
+    }
+
+    public void removeEmployee(long employeeId) {
+        Employee emp = em.find(Employee.class, employeeId);
+        if (emp != null) {
+            em.remove(emp);
+        }
+    }
+
+    public Employee changeEmployeeName(long id, String firstName) {
+        Employee emp = em.find(Employee.class, id);
+        if (emp != null) {
+            EntityTransaction transaction = em.getTransaction();
+            transaction.begin();
+            emp.setFirstName(firstName);
+            transaction.commit();
+        }
+        return emp;
+    }
+
+    public Employee findEmployeeById(long id) {
+        Employee employee = em.find(Employee.class, id);
+        if (employee != null) {
+            System.out.println(employee.getFirstName());
+        } else {
+            System.out.println("Employee not found");
+        }
+        return employee;
+    }
 }
